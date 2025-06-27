01b292c4135e44f6b405f038d6635e9aa3e404cc
ebnybuniad <ebnybuniad@hmail.com>
2025-06-27 18:19:43 +0600

    aded user info

--- src/pages/Dashboard/MyAccount/MyAccount.jsx
+++ src/pages/Dashboard/MyAccount/MyAccount.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { use } from 'react';
+import AuthContext from '../../../Context/AuthContext';
 
 const MyAccount = () => {
+
+    const { user } = use(AuthContext);
+    console.log(user)
+
     return (
         <div>
             My Account
+            <div className='w-96 mx-auto lg:mx-0 rounded-2xl p-2 my-5 shadow text-center'>
+                <img className='border border-gray-300 rounded-full mx-auto w-20 mb-5' src={user.photoURL} alt="" />
+                <p>Name : {user?.displayName}</p>
+                <p>Email : {user?.email}</p>
+            </div>
         </div>
     );
 };
